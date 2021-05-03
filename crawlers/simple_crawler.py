a6ae467776ce84d2cc09cb16e9cac0e7b3d149c3
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-03 21:45:51 +0430

    made it more generic, ignore the comments, I'm not gonna delete those

--- crawlers/simple_crawler.py
+++ crawlers/simple_crawler.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import json 
 import requests
 
 url = 'https://www.farsnews.ir'
@@ -7,18 +8,31 @@ response = requests.get(url)
 content = BeautifulSoup(response.text, 'lxml')
 
 links = content.findAll('a')
-for link in links:
-    if 'http' not in link['href']:
-        link = url + link['href']
-        res = requests.get(link)
-        content = BeautifulSoup(res.text, 'lxml')
-        title = content.find('title')
+paragraphs = content.findAll('p')
 
-        print({'link' : link, 'title' : title.text})
-    else:
-        link = link['href']
-        res = requests.get(link)
-        content = BeautifulSoup(res.text, 'lxml')
-        title = content.find('title')
+description = ' '
+for paragraph in paragraphs:
+    description += paragraph.text.strip().replace('\n', ' ')
 
-        print({'link' : link , 'title' : title.text})
+result = {
+    'link' : url, 
+    'links_found': len(links),
+    'description': description
+}
+
+print(json.dumps(result, indent=2))
+# for link in links:
+#     if 'http' not in link['href']:
+#         link = url + link['href']
+#         res = requests.get(link)
+#         content = BeautifulSoup(res.text, 'lxml')
+#         title = content.find('title')
+
+#         print({'link' : link, 'title' : title.text})
+#     else:
+#         link = link['href']
+#         res = requests.get(link)
+#         content = BeautifulSoup(res.text, 'lxml')
+#         title = content.find('title')
+
+#         print({'link' : link , 'title' : title.text})
