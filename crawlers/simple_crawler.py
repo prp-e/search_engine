60882038f9775d0904e27df2b8bd8b005894e2cc
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-03 21:47:13 +0430

    some news to crawl

--- crawlers/simple_crawler.py
+++ crawlers/simple_crawler.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import json 
 import requests
 
-url = 'https://www.farsnews.ir'
+url = 'https://www.farsnews.ir/news/14000213000859/%D9%85%D9%82%D8%A7%D9%85-%D8%AF%D9%88%D9%84%D8%AA-%D8%AA%D8%B1%D8%A7%D9%85%D9%BE-%D8%A8%D8%A7%D8%B1%D9%87%D8%A7-%D8%AE%D9%88%D8%A7%D8%B3%D8%AA%D8%A7%D8%B1-%D9%88%D8%B3%D8%A7%D8%B7%D8%AA-%D9%81%D8%B1%D8%A7%D9%86%D8%B3%D9%87-%D9%88-%D8%A7%D9%86%DA%AF%D9%84%DB%8C%D8%B3-%D8%A8%D8%B1%D8%A7%DB%8C-%D9%85%D8%B0%D8%A7%DA%A9%D8%B1%D9%87-%D8%A8%D8%A7'
 
 response = requests.get(url) 
 content = BeautifulSoup(response.text, 'lxml')
