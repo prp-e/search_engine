b3f33c1bf288cee0ac35ba24d87a0459a50c4e76
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-03 21:37:13 +0430

    Worked on crawling a little bit better

--- crawlers/simple_crawler.py
+++ crawlers/simple_crawler.py
@@ -1,11 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.farsnews.ir/'
+url = 'https://www.farsnews.ir'
 
 response = requests.get(url) 
 content = BeautifulSoup(response.text, 'lxml')
 
 links = content.findAll('a')
 for link in links:
-    print(link['href'])
+    if 'http' not in link['href']:
+        link = url + link['href']
+        res = requests.get(link)
+        content = BeautifulSoup(res.text, 'lxml')
+        title = content.find('title')
+
+        print({'link' : link, 'title' : title.text})
+    else:
+        link = link['href']
+        res = requests.get(link)
+        content = BeautifulSoup(res.text, 'lxml')
+        title = content.find('title')
+
+        print({'link' : link , 'title' : title.text})
