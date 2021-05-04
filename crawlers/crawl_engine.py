dcc053edd2817607526bb0484f66a55f4c0d9816
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-04 13:15:13 +0430

    doing something for the internal links

--- crawlers/crawl_engine.py
+++ crawlers/crawl_engine.py
@@ -37,6 +37,10 @@ def crawl(url, depth):
         try:
             if 'http' in link['href']:
                 crawl(link['href'], depth - 1)
+            
+            if 'http' not in link['href']:
+                link = url + '/' + link['href']
+                crawl(link, depth - 1)
         except KeyError as e:
             pass
 
